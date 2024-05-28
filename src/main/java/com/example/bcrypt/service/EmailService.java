fef22a709957b925061d900a190f5f0ab11f96b5
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-05-28 15:13:59 +0200

    Update EmailService.java

--- src/main/java/com/example/bcrypt/service/EmailService.java
+++ src/main/java/com/example/bcrypt/service/EmailService.java
@@ -1,1 +1,30 @@
+package com.example.bcrypt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    /*
+         JavaMailSender provides a robust and flexible way to integrate email functionality into Spring applications,
+          leveraging the power of the JavaMail API with the simplicity and configurability of the Spring framework.
+     */
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        /*
+            SimpleMailMessage is a convenient utility for handling basic email requirements within a Spring application,
+             making it easy to send plain text emails with minimal setup. For more complex email needs
+                     (e.g., HTML content, attachments), MimeMessage is typically used instead.
+         */
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
