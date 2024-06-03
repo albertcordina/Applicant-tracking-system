c1606e8e0dfa086bd6f453baf66a9f6aebaaf6d1
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-03 16:19:47 +0200

    Update MessageType.java

--- src/main/java/com/example/bcrypt/model/MessageType.java
+++ src/main/java/com/example/bcrypt/model/MessageType.java
@@ -1,5 +1,11 @@
 package com.example.bcrypt.model;
-
+/*
+     enum (short for enumeration) is a special data type that enables for a variable to be a set of predefined constants.
+                     The enum constants are implicitly static and final, and they represent a collection of fixed values.
+     This is useful for defining a set of related constants, such as days of the week, months of the year, or specific types of operations.
+     MessageType is used to define a set of constants that represent different types of messages in a communication context.
+     Using enum in this way provides clarity and type safety, ensuring that only valid message types are used throughout the application.
+ */
 public enum MessageType {
 
     CHAT,
