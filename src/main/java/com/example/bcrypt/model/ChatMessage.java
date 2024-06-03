80387ab8d4d3d0b8230518e40c600966c08fd092
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-03 16:11:42 +0200

    Update ChatMessage.java

--- src/main/java/com/example/bcrypt/model/ChatMessage.java
+++ src/main/java/com/example/bcrypt/model/ChatMessage.java
@@ -1,1 +1,17 @@
+package com.example.bcrypt.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+public class ChatMessage {
+
+    @Getter
+    private MessageType type;
+
+    @Getter
+    private String message;
+
+    @Getter
+    private String sender;
+}
