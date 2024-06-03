0de25a4f88eeeb02e512fac9ae471027ddca6752
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-03 16:39:53 +0200

    Update WebSocketMessageConfig.java

--- src/main/java/com/example/bcrypt/web/WebSocketMessageConfig.java
+++ src/main/java/com/example/bcrypt/web/WebSocketMessageConfig.java
@@ -1,1 +1,23 @@
+package com.example.bcrypt.web;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketMessageConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
