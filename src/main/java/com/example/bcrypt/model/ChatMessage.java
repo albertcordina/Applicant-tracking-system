be68216b330c9c66b0b8461424274220acedea97
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-04 11:56:58 +0200

    Update ChatMessage.java

--- src/main/java/com/example/bcrypt/model/ChatMessage.java
+++ src/main/java/com/example/bcrypt/model/ChatMessage.java
@@ -3,15 +3,27 @@ package com.example.bcrypt.model;
 import lombok.Builder;
 import lombok.Getter;
 
+/* 
+      By eliminating boilerplate code, Spring Boot Lombok enhances
+      productivity, readability, and maintainability of projects.
+
+ @Builder design pattern allows for a flexible and readable way to construct instances of the class.
+   The @Builder annotation automatically generates a static inner builder class with methods for 
+       setting the fields and a build() method to create an instance of the ChatMessage class.  
+*/
+
 @Builder
 public class ChatMessage {
 
+    // Generates a getter method for the 'type' field
     @Getter
     private MessageType type;
 
+    // for the 'message' field
     @Getter
     private String message;
 
+    // for the 'sender' field
     @Getter
     private String sender;
 }
