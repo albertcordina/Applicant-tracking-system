736fc10e928f9ad11cacd579fd16ee7bede19806
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-04 15:24:25 +0200

    Update WebSocketMessageConfig.java

--- src/main/java/com/example/bcrypt/web/WebSocketMessageConfig.java
+++ src/main/java/com/example/bcrypt/web/WebSocketMessageConfig.java
@@ -6,20 +6,20 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-// Annotates this class as a configuration class that contains @Bean definitions
+/* Annotates this class as a configuration class that contains @Bean definitions */
 @Configuration
-// Enables WebSocket message handling, backed by a message broker
+/* Enables WebSocket message handling, backed by a message broker */
 @EnableWebSocketMessageBroker
 public class WebSocketMessageConfig implements WebSocketMessageBrokerConfigurer {
 
-    // Registers STOMP (Simple Text Oriented Messaging Protocol) endpoints and maps each to a specific URL
+    /* Registers STOMP (Simple Text Oriented Messaging Protocol) endpoints and maps each to a specific URL */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Adds an endpoint that clients will use to connect to the WebSocket server with SockJS fallback options
         registry.addEndpoint("/chat").withSockJS();
     }
 
-    // Configures the message broker options
+    /* Configures the message broker options */
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
         // Sets the application destination prefix for messages bound for @MessageMapping-annotated methods
