97391d9eb788baa42ee86c4a50ca1483da537351
Albert Cordina <137774947+albertcordina@users.noreply.github.com>
2024-06-11 16:42:04 +0200

    Update ApplicantController.java

--- src/main/java/com/example/bcrypt/controller/ApplicantController.java
+++ src/main/java/com/example/bcrypt/controller/ApplicantController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -35,46 +36,59 @@ public class ApplicantController {
     }
 
     @GetMapping("/news")
-    public String news() { return "news";}
+    public String news() {
+        return "news";
+    }
 
 //----------------------------- Applicant's Deletion request ---------------------------------
 
-    @GetMapping("/applicantsDeletionRequest") // for admin review.
-    public String showApplicantsDeletionRequests(Model model) {
-        List<Applicants> applicants = applicantService.findApplicantsByDeletion();
-        if (applicants.isEmpty()) {
-            return "not_found_page";
-        }
-        model.addAttribute("applicants", applicants);
-        return "account_removal_list";
-    }
-
     @GetMapping("/removeAccountRequestPage") // for applicant to send the request.
-        public String removeAccountRequestGetPage (@RequestParam("username") String username, Model model) {
+    public String removeAccountRequestGetPage(@RequestParam("username") String username, Model model) {
         Applicants applicants = applicantService.findApplicantByUsername(username);
         model.addAttribute("applicants", applicants);
         return "account_removal_request";
     }
 
-    @GetMapping("/removeAccountRequestSend")
+    @GetMapping("/removeAccountRequestSend") // sending the removal of the account request to admin.
     public String removeAccountRequestSend(@ModelAttribute("username") @Valid Applicants applicants, BindingResult bindingResult) {
-        if (bindingResult.hasErrors() || !applicants.isDeletion()) { // if error or applicant has already sent the request.
-            return "error";
-        }
+        if (bindingResult.hasErrors()) { return "error"; }// if error .
         applicantService.saveApplicant(applicants);
         return "success_page";
     }
+
+    @GetMapping("/applicantsDeletionRequest") // for admin review.
+    public String showApplicantsDeletionRequests(Model model) {
+        List<Applicants> applicants = applicantService.findApplicantsByDeletion();
+        if (applicants.isEmpty()) { // applicants.isEmpty() is to check if a LIST of applicants is empty.
+            return "not_found_page";
+        }
+        model.addAttribute("applicants", applicants);
+        return "account_removal_list";
+    }
+    
+    @PostMapping("/removeAccountDeletionRequest") // removal of the account deletion request managed by the admin.
+    public String removeAccountDeletionRequest(@RequestParam("username") String username, Model model) {
+        Applicants applicant = applicantService.findApplicantByUsername(username);
+        // applicant == null is to check if a SINGLE applicant object is null.
+        if (applicant == null) {return "not_found_page"; } // Handle the case where the applicant is not found.
+        applicant.setDeletion(false); // Set to 'false' the 'deletion' boolean.
+        applicantService.saveApplicant(applicant);
+        model.addAttribute("applicants", applicantService.findAllApplicants()); // Update the model with all applicants
+
+        return "success_page";
+    }
+
 //------------------------------------- update the Applicant's data ---------------------------------------
 
     @GetMapping("/updateApplicantGetPage")
-    public String updateApplicantPage (@RequestParam("username") String username, Model model) {
+    public String updateApplicantPage(@RequestParam("username") String username, Model model) {
         Applicants applicants = applicantService.findApplicantByUsername(username);
         model.addAttribute("applicants", applicants);
         return "update_form";
     }
 
     @GetMapping("/updateApplicant")
-    public String updateApplicant (@ModelAttribute("username") @Valid Applicants applicants, BindingResult bindingResult) {
+    public String updateApplicant(@ModelAttribute("username") @Valid Applicants applicants, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "update_form";
         }
